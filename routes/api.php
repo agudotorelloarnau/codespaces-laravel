78677f89ee9d50c737e2ff238c03152167f9ce8b
arnauagudofpllefia <arnagudo05@gmail.com>
2026-03-16 15:12:11 +0000

    .

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\StudentController;
 use App\Http\Controllers\Api\SeriesController;
 use App\Http\Controllers\Api\MovieController;
 use App\Http\Controllers\AuthController;
@@ -23,12 +22,9 @@ Route::get('/series/{id}', [SeriesController::class, 'show']);
 
 //protegidas
 Route::middleware([IsUserAuth::class])->group(function () {
-    Route::get('/students', [StudentController::class, 'index']);
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/me', [AuthController::class, 'getUser']);
-    //Route::post('personajes', [MovieController::class, 'addPersonaje']);
-
-
+    
     Route::post('/movies', [MovieController::class, 'store']);
     Route::put('/movies/{id}', [MovieController::class, 'update']);
     Route::delete('/movies/{id}', [MovieController::class, 'destroy']);
@@ -38,13 +34,3 @@ Route::middleware([IsUserAuth::class])->group(function () {
     Route::put('/series/{id}', [SeriesController::class, 'update']);
     Route::delete('/series/{id}', [SeriesController::class, 'destroy']);
 });
-
-Route::get('/students/{id}', [StudentController::class, 'estudiante2']);
-
-//Route::post('/students', function () { return 'Crear student'; });
-
-//Route::put('/students/{id}', function () { return 'Actualizar student'; });
-
-//Route::delete('/students/{id}', function () { return 'Eliminar student'; });
-
-//Route::get('/students/{id}', function () {return 'Lista de Students'; });
