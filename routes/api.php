81539bfa736f168c7172ab79bff28648cf64c27e
arnauagudofpllefia <arnagudo05@gmail.com>
2026-03-16 14:42:22 +0000

    feat: Añadir rutas para obtener y gestionar películas y series

--- routes/api.php
+++ routes/api.php
@@ -14,10 +14,12 @@ use App\Http\Middleware\IsUserAuth;
 
 //publicas
 Route::post('/register', [AuthController::class, 'register']);
-
 Route::post('/login', [AuthController::class, 'login']);
 
-
+Route::get('/movies', [MovieController::class, 'index']);
+Route::get('/movies/{id}', [MovieController::class, 'show']);
+Route::get('/series', [SeriesController::class, 'index']);
+Route::get('/series/{id}', [SeriesController::class, 'show']);
 
 //protegidas
 Route::middleware([IsUserAuth::class])->group(function () {
@@ -25,6 +27,16 @@ Route::middleware([IsUserAuth::class])->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/me', [AuthController::class, 'getUser']);
     //Route::post('personajes', [MovieController::class, 'addPersonaje']);
+
+
+    Route::post('/movies', [MovieController::class, 'store']);
+    Route::put('/movies/{id}', [MovieController::class, 'update']);
+    Route::delete('/movies/{id}', [MovieController::class, 'destroy']);
+
+
+    Route::post('/series', [SeriesController::class, 'store']);
+    Route::put('/series/{id}', [SeriesController::class, 'update']);
+    Route::delete('/series/{id}', [SeriesController::class, 'destroy']);
 });
 
 Route::get('/students/{id}', [StudentController::class, 'estudiante2']);
