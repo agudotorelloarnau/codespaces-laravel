91a2ede934bb9da1302bfd922e885b81802073fe
agudotorelloarnau <agudotorelloarnau@gmail.com>
2026-03-27 12:10:02 +0000

    .

--- routes/api.php
+++ routes/api.php
@@ -19,14 +19,16 @@ Route::get('/categories/{id}', [CategoryController::class, 'show']);
 Route::middleware([IsUserAuth::class])->group(function () {
     Route::get('/me', [AuthController::class, 'me']);
     Route::post('/logout', [AuthController::class, 'logout']);
+
+    Route::middleware([IsAdmin::class])->group(function () {
+        Route::post('/products', [ProductController::class, 'store']);
+        Route::put('/products/{id}', [ProductController::class, 'update']);
+        Route::delete('/products/{id}', [ProductController::class, 'destroy']);
+
+        Route::post('/categories', [CategoryController::class, 'store']);
+        Route::put('/categories/{id}', [CategoryController::class, 'update']);
+        Route::delete('/categories/{id}', [CategoryController::class, 'destroy']);
+    });
 });
 
-Route::middleware([IsUserAuth::class, IsAdmin::class])->group(function () {
-    Route::post('/products', [ProductController::class, 'store']);
-    Route::put('/products/{id}', [ProductController::class, 'update']);
-    Route::delete('/products/{id}', [ProductController::class, 'destroy']);
 
-    Route::post('/categories', [CategoryController::class, 'store']);
-    Route::put('/categories/{id}', [CategoryController::class, 'update']);
-    Route::delete('/categories/{id}', [CategoryController::class, 'destroy']);
-});
