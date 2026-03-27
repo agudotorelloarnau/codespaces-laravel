75ad2f0cb956af15733cf7a5cc4f531bc6434f52
agudotorelloarnau <agudotorelloarnau@gmail.com>
2026-03-27 11:48:33 +0000

    .

--- app/Http/Middleware/IsUserAuth.php
+++ app/Http/Middleware/IsUserAuth.php
@@ -6,29 +6,19 @@ use Closure;
 use Illuminate\Http\Request;
 use Tymon\JWTAuth\Facades\JWTAuth;
 use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Exceptions\TokenInvalidException;
+use Tymon\JWTAuth\Exceptions\TokenExpiredException;
 
 class IsUserAuth
 {
+
     public function handle(Request $request, Closure $next)
-    {
-        try {
-            $user = JWTAuth::parseToken()->authenticate();
-
-            if (! $user) {
-                return response()->json(['message' => 'User not found'], 401);
-            }
-        } catch (JWTException $exception) {
-            if ($exception instanceof \Tymon\JWTAuth\Exceptions\TokenInvalidException) {
-                return response()->json(['message' => 'Token invalid'], 401);
-            } elseif ($exception instanceof \Tymon\JWTAuth\Exceptions\TokenExpiredException) {
-                return response()->json(['message' => 'Token expired'], 401);
-            } else {
-                return response()->json(['message' => 'Token not provided'], 401);
-            }
-        }
-
-        $request->setUserResolver(fn () => $user);
-
-        return $next($request);
+{
+    if (! $user = auth('api')->user()) {
+        return response()->json(['message' => 'Unauthorized Invalid Token'], 401);
     }
+
+    $request->setUserResolver(fn () => $user);
+    return $next($request);
+}
 }
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProductController;
+use App\Http\Middleware\IsUserAuth;
+use App\Http\Middleware\IsAdmin;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
@@ -14,12 +16,12 @@ Route::get('/products/{id}', [ProductController::class, 'show']);
 Route::get('/categories', [CategoryController::class, 'index']);
 Route::get('/categories/{id}', [CategoryController::class, 'show']);
 
-Route::middleware([App\Http\Middleware\IsUserAuth::class])->group(function () {
+Route::middleware([IsUserAuth::class])->group(function () {
     Route::get('/me', [AuthController::class, 'me']);
     Route::post('/logout', [AuthController::class, 'logout']);
 });
 
-Route::middleware([App\Http\Middleware\IsUserAuth::class, App\Http\Middleware\IsAdmin::class])->group(function () {
+Route::middleware([IsUserAuth::class, IsAdmin::class])->group(function () {
     Route::post('/products', [ProductController::class, 'store']);
     Route::put('/products/{id}', [ProductController::class, 'update']);
     Route::delete('/products/{id}', [ProductController::class, 'destroy']);
